62d831b284aa0edff51a229e1559110798b7f587
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-12-08 08:27:43 -0300

    finalizando aula de pegar elementos a apartir de uma tag especificas

---
+++ getElementByTagName/script.js
@@ -0,0 +1,11 @@
+console.log(
+    document.getElementsByTagName("div")[2].innerHTML
+);
+
+console.log(
+    document.getElementsByTagName("h1")[0].innerHTML
+);
+
+console.log(
+    document.getElementsByTagName("p")[0].innerHTML
+)
