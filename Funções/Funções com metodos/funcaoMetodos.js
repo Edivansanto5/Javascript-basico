7497ece5f5447b3015cd67fcf910c6f2d8605756
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-02-06 18:52:19 -0300

    criando uma funcao com metados

---
+++ Funções/Funções com metodos/funcaoMetodos.js
@@ -0,0 +1,14 @@
+// quando combinamos funções com  objetos,obtemos os metados
+// todos os objetos em javascript possuem metados
+// vejamos o exemplo abaixo
+
+let a = [] // variavel a recebendo um array vazio
+a.push(1,2,3,4,5) // variavel a recebendo elementos atraves do metado PUSH()
+a.reverse() // invertendo os valores dos atributos com o metado REVERSE()
+
+console.log(a); 
+
+
+
+// Tambem podemos criar nossos próprios metados. A palavra 'THIS' se refere ao objeto onde esta se referindo
+// vejamos ha um exemplo
